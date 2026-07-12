04625afd05362c677d000d2523128f0cb82de8f0
Jefferson Yepez <jeffersonyepez621@gmail.com>
2026-07-12 10:29:10 -0500

    feat(tickets): add RabbitMQ audit event publisher

--- tickets/src/tickets/tickets.module.ts
+++ tickets/src/tickets/tickets.module.ts
@@ -4,6 +4,7 @@ import { AuthModule } from '../auth/auth.module';
 import { AssignmentsClient } from './clients/assignments.client';
 import { VehiclesClient } from './clients/vehicles.client';
 import { ZonesClient } from './clients/zones.client';
+import { EventPublisher } from './event-published.service';
 import { Ticket } from './entities/ticket.entity';
 import { TicketsController } from './tickets.controller';
 import { TicketsService } from './tickets.service';
@@ -11,7 +12,13 @@ import { TicketsService } from './tickets.service';
 @Module({
   imports: [TypeOrmModule.forFeature([Ticket]), AuthModule],
   controllers: [TicketsController],
-  providers: [TicketsService, ZonesClient, VehiclesClient, AssignmentsClient],
+  providers: [
+    TicketsService,
+    ZonesClient,
+    VehiclesClient,
+    AssignmentsClient,
+    EventPublisher,
+  ],
   exports: [TicketsService],
 })
 export class TicketsModule {}
